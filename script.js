f55b2f33f33139957a2d65d71f884d5e9eb3e928
jhonatan583 <jhoninhaoficial@gmail.com>
2025-02-01 10:08:10 -0300

    adicionado math randon

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function generatenumber(){}
+function generatenumber() {
 
-console.log("me chamou?")
+    const min = Math.ceil (document.querySelector (".input-min").value)
+    const max = Math.floor (document.querySelector(".input-max").value)
+    
+   const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert(result)
+
+}
